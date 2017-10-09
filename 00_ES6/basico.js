02f9c7535ed471885a596a9cd077223cd79a5c9f
alce65 <alce65@hotmail.es>
2017-10-09 16:57:30 +0200

    ES6

---
+++ 00_ES6/basico.js
@@ -0,0 +1,26 @@
+// Ejemplo de código en ES6
+
+var data = [{precio: 12}, {precio: 34}, {precio: 19}]; 
+
+// data.forEach(function() {})
+
+// Arrow : () => {}
+// (a,b) => a+b
+
+data.forEach( elem => {
+if (true) {
+
+	const iva = 1.16
+	let precioFinal = elem.precio * iva
+	
+	
+	console.log(`
+	Oferta: 
+	El precio final es ${precioFinal}`);
+}
+
+ 
+//console.log (iva)
+//console.log (precioFinal);
+
+});
